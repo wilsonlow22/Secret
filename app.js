2b703f16319d20526f498cdc596c5edc323f5ce3
wilson <wilsonlow006@gmail.com>
2019-12-02 23:11:01 +0800

    Add Evironment Vars2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
+ 
 const secret = process.env.SECRET;
 
 userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
